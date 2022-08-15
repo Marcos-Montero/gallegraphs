9fafacc71c9504d4670d552dbd446b6c56c89bb9
Marcos <marcos@nomoko.world>
2022-08-15 18:07:50 +0200

    style header

---
+++ frontend/src/components/Header/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 20px;
+  border-radius: 8px;
+  border-left: 1px solid black;
+  border-right: 1px solid black;
+`
