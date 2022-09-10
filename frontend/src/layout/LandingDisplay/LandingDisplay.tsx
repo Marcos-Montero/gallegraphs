ce63f6d65626ffbe2d413c286f2ebcd7c0f8749a
marcos_nomoko <marcos@nomoko.world>
2022-09-10 18:13:50 +0200

    fix types

--- frontend/src/layout/LandingDisplay/LandingDisplay.tsx
+++ frontend/src/layout/LandingDisplay/LandingDisplay.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from "react";
 import { LandingContainer, Message } from "./style";
 
-export const LandingDisplay: FC = ({ children }: { children?: ReactNode }) => {
+export const LandingDisplay = ({ children }: { children: ReactNode }) => {
   return (
     <LandingContainer>
       <Message>{children}</Message>
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,2 @@
-/// <reference types="vite/client vite-plugin-svgr/client" />
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
